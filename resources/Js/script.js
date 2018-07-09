4c89cba4622410d1ae28ea6081e10152d5b870ff
Thorley <thorley.eric@gmail.com>
2018-07-09 19:57:43 -0400

    added chart.js blank template

--- resources/Js/script.js
+++ resources/Js/script.js
@@ -10,3 +10,24 @@
   firebase.initializeApp(config);
 
   var database = firebase.database();
+
+//leaving space for whatever we use the database for
+
+
+//blank chart template from chart.js documentation
+var ctx = document.getElementById("calories").getContext('2d'); // whatever name we are going to give the calories div
+var caloriesChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      //data pulled from the API should populate here
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
