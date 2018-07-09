19249f9d9ab396d15515c9f8fedcad781b9c1988
Thorley <thorley.eric@gmail.com>
2018-07-09 19:05:36 -0400

    added firebasse initialization

---
+++ resources/Js/script.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC2DnXzYbdUXxn8EHwAm4bnn6Hyytm6clg",
+    authDomain: "visualize-me-database.firebaseapp.com",
+    databaseURL: "https://visualize-me-database.firebaseio.com",
+    projectId: "visualize-me-database",
+    storageBucket: "",
+    messagingSenderId: "44390363684"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
