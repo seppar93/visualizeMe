b4e13ffb2cb590f2ed2ccbeb91749e83eef31452
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-07-16 12:31:28 -0400

    restructred

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const brain = require( 'brain.js');
-var net = new brain.NeuralNetwork();
+const brain = require( 'brain.js'); // use of brain.js libary
+var net = new brain.NeuralNetwork(); // setting up neural network
 
-var mfp = require("mfp");
-const fs = require('fs');
-
-
+var mfp = require("mfp"); // setting up variable to use MFP module
+const fs = require('fs'); // set up fire writing module
 
 
 mfp.fetchDateRange('Charoolz', '2018-01-15', '2018-07-01', ['calories', 'protein', 'carbs', 'fat'], function(dataSet){
   // console.log(data['data']);
-  console.log(dataSet);
+  // console.log(dataSet);
 
   let calories = getData(dataSet,getCalories)
   // console.log(dataTrain(calories,2000));
-  net.train(dataTrain(calories,2500));
-  var output = net.run({ calories: 1800});
+  net.train(dataTrain(calories,2500)); // calorie limit and the ammount eatin over the years
+  var output = net.run({ calories: 1800}); // calorie limit inputed for the week
   // console.log(output);
 
-  dataSet.trainedOuput = output;
+  dataSet.trainedOuput = output; // resule
 
-  fs.writeFileSync("dataset.js",'var profileData='+JSON.stringify(dataSet, null ,2));
+  fs.writeFileSync("dataset.js",'var profileData='+JSON.stringify(dataSet, null ,2)); // writing javascript file formated
 });
 
 // setTimeout(function () {
   // console.log(dataSet);
 
-  function getData(dataset,getFunction) {
+  function getData(dataset,getFunction) { // higher orderd function taking data and function
     for(let key in dataset) {
       if (key === "data") {
         let info = dataset[key];
@@ -45,7 +43,7 @@ mfp.fetchDateRange('Charoolz', '2018-01-15', '2018-07-01', ['calories', 'protein
 
   }
 
-  function dataTrain(arrayCalories,calorieLevel) {
+  function dataTrain(arrayCalories,calorieLevel) { // function takes all calories and the level to look for 
     let whatIneed = [];
     // console.log(arrayCalories);
 
