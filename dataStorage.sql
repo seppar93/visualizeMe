54992455e3438e13b958cf2a08f56691d570f215
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-08-12 19:17:20 -0400

    added sql

---
+++ dataStorage.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE dataStorage;
+USE dataStorage;
+
+CREATE TABLE macros (
+	calories INTEGER(11) AUTO_INCREMENT NOT NULL,
+  protiens INTEGER(11) AUTO_INCREMENT NOT NULL,
+  carbs INTEGER(11) AUTO_INCREMENT NOT NULL,
+  fats INTEGER(11) AUTO_INCREMENT NOT NULL,
+
+);
+-- INSERT INTO products (calories, protiens, carbs, fats)
+-- VALUES ('2000', '30', 40, 20);
